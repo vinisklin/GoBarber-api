7390f4a560a38cb5d5d2869a0e28da05d2b051a7
vinisklin <vinisklin@gmail.com>
2020-06-25 10:25:21 -0300

    Created database environment

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
-
 import routes from './routes';
+
+import './database';
 
 const app = express();
 
@@ -8,7 +9,5 @@ app.use(express.json());
 app.use('/', routes);
 
 app.listen(3333, () => {
-  console.log('Server started on port 3333!');
+  console.log('🚀 Server started on port 3333!');
 });
-
-// VER SE ESTÁ CORRETO
