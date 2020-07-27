6f8383ebd6064dc9c7b5eb2f94753a33b9f45368
vinisklin <vinisklin@gmail.com>
2020-07-27 20:48:25 -0300

    Create: List providers functionality

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 
 import appointmentsRoutes from '@modules/appointments/infra/http/routes/appointments.routes';
+import providersRoutes from '@modules/appointments/infra/http/routes/providers.routes';
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
@@ -9,6 +10,7 @@ import profileRouter from '@modules/users/infra/http/routes/profile.routes';
 const routes = Router();
 
 routes.use('/appointments', appointmentsRoutes);
+routes.use('/providers', providersRoutes);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
