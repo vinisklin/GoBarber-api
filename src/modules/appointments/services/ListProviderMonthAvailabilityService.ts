17dcc7d3cba2b01c48c45eed6b351e0992436854
vinisklin <vinisklin@gmail.com>
2020-07-29 20:53:40 -0300

    Create: List providers availability controllers and routes

--- src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -19,7 +19,7 @@ type IResponse = Array<{
 @injectable()
 class ListProviderMonthAvailabilityService {
   constructor(
-    @inject('ApppointmentsRepository')
+    @inject('AppointmentsRepository')
     private appointmentsRepository: IAppointmentsRepository,
   ) {}
 
