db2660bc8db612cecbc8c09f65cc17c1958b5f5a
vinisklin <vinisklin@gmail.com>
2020-07-30 11:49:05 -0300

    Create: List Provider Appointments functionality

---
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderAppointments from '@modules/appointments/services/ListProviderAppointmentsService';
+
+export default class ProviderAppointmentsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const provider_id = request.user.id;
+    const { day, month, year } = request.body;
+
+    const createAppointment = container.resolve(ListProviderAppointments);
+
+    const appointments = await createAppointment.execute({
+      provider_id,
+      day,
+      month,
+      year,
+    });
+
+    return response.json(appointments);
+  }
+}
