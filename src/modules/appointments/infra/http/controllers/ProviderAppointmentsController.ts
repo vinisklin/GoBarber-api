99c12cc754b1e457924361fa167b05bef52947c4
vinisklin <vinisklin@gmail.com>
2020-08-17 14:12:17 -0300

    Fix: Routes and services to work with front-end

--- src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
+++ src/modules/appointments/infra/http/controllers/ProviderAppointmentsController.ts
@@ -1,22 +1,23 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import ListProviderAppointments from '@modules/appointments/services/ListProviderAppointmentsService';
 
 export default class ProviderAppointmentsController {
   public async index(request: Request, response: Response): Promise<Response> {
     const provider_id = request.user.id;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const createAppointment = container.resolve(ListProviderAppointments);
 
     const appointments = await createAppointment.execute({
       provider_id,
-      day,
-      month,
-      year,
+      day: Number(day),
+      month: Number(month),
+      year: Number(year),
     });
 
-    return response.json(appointments);
+    return response.json(classToClass(appointments));
   }
 }
